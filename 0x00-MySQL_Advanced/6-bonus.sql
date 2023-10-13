9078d8b984e93b5fa60787a68f776c5f5d86ddcf
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-13 12:01:42 +0300

    6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,9 +8,21 @@ CREATE PROCEDURE AddBonus
 	IN score FLOAT
 )
 BEGIN
+	DECLARE project_id INT;
+
+	-- Check if the project already exists, if not create a new project
+	SET project_id = (SELECT id FROM projects WHERE name = project_name);
+
+	IF project_id IS NULL THEN
+		INSERT INTO projects (name) VALUES (project_name);
+		SET project_id = LAST_INSERT_ID();
+	END IF;
+	
 	UPDATE corrections
-	SET corrections.score = score
-	WHERE project_id = (SELECT id FROM projects WHERE name = (SELECT name FROM users WHERE id = user_id));
+	SET score = score
+	WHERE user_id = user_id AND user_id = user_id
+
+
 END$$
 DELIMITER ;
 
