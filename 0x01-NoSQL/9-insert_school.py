b48bff97250a3cf085580d7d295cd771edbe233a
Mbugua <mwaurathealex@gmail.com>
2024-02-21 13:14:47 +0300

    insert document based on kwargs

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Module contains the function insert_school(mongo_collection, **kwargs)"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts new document into collection based on kwargs"""
+    inserted_doc = mongo_collection.insert_one(kwargs)
+    return inserted_doc.inserted_id
