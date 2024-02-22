f0312a49e1df028fc332bbcda8b1c78869f3f69c
Mbugua <mwaurathealex@gmail.com>
2024-02-22 08:21:10 +0300

    docs

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Module contains a Python script that
-provides some stats about Nginx logs stored in MongoDB
+"""Provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
