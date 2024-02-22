5a68175e4426de7c2c51ea542831cba7c6c143ce
Mbugua <mwaurathealex@gmail.com>
2024-02-22 06:18:13 +0300

    where can I learn python

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+from pymongo.collection import Collection
+"""School by topic module"""
+
+
+def schools_by_topic(mongo_collection: Collection, topic):
+    """returns the list of school having a specific topic"""
+    documents = []
+    for document in mongo_collection.find({"topics": topic}):
+        documents.append(document)
+    return documents
