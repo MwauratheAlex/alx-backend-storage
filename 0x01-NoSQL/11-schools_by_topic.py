a594e4bb6d2cde1f4da98aca5a838429a59b239c
Mbugua <mwaurathealex@gmail.com>
2024-02-22 07:46:06 +0300

    remove anotation

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """School by topic module"""
-from pymongo.collection import Collection
 
 
-def schools_by_topic(mongo_collection: Collection, topic):
+def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
     documents = []
     for document in mongo_collection.find({"topics": topic}):
