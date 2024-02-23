c1dc33f9438998dcd531d8bb6aa884f561e996a0
Mbugua <mwaurathealex@gmail.com>
2024-02-23 13:54:55 +0300

    remove global redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,37 +5,24 @@ import requests
 import redis
 from functools import wraps
 
-redis_instance = redis.Redis()
-
-
-def count_calls(method: Callable) -> Callable:
-    """counts how many times a url was called"""
-
-    @wraps(method)
-    def wrapper(url):
-        key = f'count:{url}'
-        redis_instance.incr(key)
-        return method(url)
-    return wrapper
-
 
 def cache_results(method: Callable) -> Callable:
     """cache results of method"""
+    redis_instance = redis.Redis()
 
     @wraps(method)
     def wrapper(url):
-        key = f'cache:{url}'
-        result = redis_instance.get(key)
+        redis_instance.incr(f'count:{url}')
+        result = redis_instance.get(f'result:{url}')
         if result is not None:
             return result.decode('utf8')
         result = method(url)
-        redis_instance.setex(key, 10, result)
+        redis_instance.setex(f'result:{url}', 10, result)
         return result
     return wrapper
 
 
 @cache_results
-@count_calls
 def get_page(url: str) -> str:
     """uses the requests module to obtain the HTML content
     of a particular URL and returns it"""
