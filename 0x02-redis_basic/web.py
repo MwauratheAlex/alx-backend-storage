0531ddd18aa87bd154c8279492fcf8bd78b4608e
Mbugua <mwaurathealex@gmail.com>
2024-02-23 13:33:56 +0300

    one count and cache

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,7 +13,6 @@ def count_calls(method: Callable) -> Callable:
 
     @wraps(method)
     def wrapper(url):
-        key = f'count:{url}'
         redis_instance.incr(key)
         return method(url)
     return wrapper
@@ -24,20 +23,23 @@ def cache_results(method: Callable) -> Callable:
 
     @wraps(method)
     def wrapper(url):
-        key = f'cache:{url}'
-        result = redis_instance.get(key)
+        result_key = f'result:{url}'
+        count_key = f'count:{url}'
+        redis_instance.incr(count_key)
+        result = redis_instance.get(result_key)
         if result is not None:
             return result.decode('utf8')
         result = method(url)
-        redis_instance.setex(key, 10, result)
-        redis_instance.set(f'count:{url}', 0)
+        redis_instance.setex(result_key, 10, result)
         return result
     return wrapper
 
 
-@count_calls
 @cache_results
 def get_page(url: str) -> str:
     """uses the requests module to obtain the HTML content
     of a particular URL and returns it"""
     return requests.get(url).text
+
+
+print(get_page('https://google.com'))
